2c7ce058fb7d36656908981f7bd4affc5199e2e3
Saad Anees <saad@pixelplusmedia.com>
2020-06-04 11:22:12 +0400

    All assets with photon sdk

---
+++ Assets/Game/Scripts/MobileInputController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.PPM.XRConference
+{
+    public class MobileInputController : MonoBehaviour
+    {
+        CharacterController characterController;
+        private Animator animator;
+
+        public FixedJoystick LeftJoystick;
+        public FixedJoystick RightJoystick;
+        public FixedButton FixedButton;
+
+        protected float CameraAngleY;
+        protected float CameraAngleSpeed = 2f;
+        protected float CameraPosY;
+        protected float CameraPosSpeed = 0.1f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            characterController = GetComponent<CharacterController>();
+
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var input = new Vector3(LeftJoystick.Horizontal, 0, LeftJoystick.Vertical);
+            var vel = Quaternion.AngleAxis(CameraAngleY + 180, Vector3.up) * input * 5f;
+
+            characterController.velocity.Set(vel.x, characterController.velocity.y, vel.z); //= new Vector3(vel.x, characterController.velocity.y, vel.z);
+
+            transform.rotation = Quaternion.AngleAxis(CameraAngleY + 180 + Vector3.SignedAngle(Vector3.forward, input.normalized + Vector3.forward * 0.001f, Vector3.up), Vector3.up);
+
+           
+            CameraAngleY += RightJoystick.Horizontal * CameraAngleSpeed;
+            Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngleY, Vector3.up) * new Vector3(0, 3, 4);
+            Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
+
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.PPM.XRConference
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region Private Fields
+
+        private Animator animator;
+
+        [SerializeField]
+        private float directionDampTime = 0.25f;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!animator)
+            {
+                return;
+            }
+
+
+
+            // deal with Jumping
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            // only allow jumping if we are running.
+            if (stateInfo.IsName("Base Layer.Run"))
+            {
+#if UNITY_STANDALONE
+                // When using trigger parameter
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    animator.SetTrigger("Jump");
+                }
+#endif
+               
+            }
+
+            // PC
+            #if UNITY_STANDALONE
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0)
+            {
+                v = 0;
+            }
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+            #endif
+        }
+
+        #endregion
+    }
+}
